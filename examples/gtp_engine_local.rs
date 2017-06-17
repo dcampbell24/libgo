4e27b89ccb8300982a844564cff21c752082a8bc
David Campbell <dcampbell24@gmail.com>
2017-06-17 11:49:11 -0400

    refactor command, add tests, add Commands trait

--- examples/gtp_engine_local.rs
+++ examples/gtp_engine_local.rs
@@ -2,10 +2,10 @@
 
 extern crate libgo;
 
-use std::io::{self, BufRead};
+use std::io;
 
 use libgo::game::Game;
-use libgo::gtp::command::Command;
+use libgo::gtp::command::Commands;
 use libgo::gtp::engine::Engine;
 
 fn main() {
@@ -15,14 +15,13 @@ fn main() {
     let mut game = Game::new();
     let stdin = io::stdin();
 
-    for line in stdin.lock().lines() {
-        if let Some(command) = Command::from_line(&line.expect("failed to read line")) {
-            let response = gtp.exec(&mut game, &command);
-            print!("{}", response);
+    for command in stdin.lock().commands() {
+        let command = command.expect("failed to read command");
+        let response = gtp.exec(&mut game, &command);
+        print!("{}", response);
 
-            if command.name == "quit" {
-                return
-            }
+        if command.name == "quit" {
+            return
         }
     }
 }
--- src/gtp/command.rs
+++ src/gtp/command.rs
@@ -1,6 +1,58 @@
+use std::io::{self, BufRead, Lines};
 
-fn preprocess_line(line: &str) -> Vec<String> {
+/// An Iterator that returns GTP commands.
+#[derive(Debug)]
+pub struct CommandsIter<B> {
+    lines: Lines<B>,
+}
+
+/// A trait extending BufRead to allow reading GTP commands from any type implementing BufRead.
+pub trait Commands: BufRead {
+    /// A method that returns an iterator over GTP commands.
+    fn commands(self) -> CommandsIter<Self> where Self: Sized {
+        CommandsIter { lines: self.lines() }
+    }
+}
+
+impl<C: Commands> Iterator for CommandsIter<C> {
+    type Item = io::Result<Command>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some(result_line) = self.lines.next() {
+            match result_line {
+                Ok(line) => if let Some(command) = Command::from_line(&line) {
+                    return Some(Ok(command));
+                },
+                Err(err) => {
+                    return Some(Err(err))
+                }
+            }
+        }
+        None
+    }
+}
+
+impl<T: BufRead> Commands for T { }
+
+// From the GTP 2 Specification Oct 2002:
+//
+//     3.1 Preprocessing
+//
+//     When a command string arrives to an engine, it is expected to perform the
+//     following four operations before any further parsing takes place:
+//
+//     1. Remove all occurences of CR and other control characters except for HT and LF.
+//     2. For each line with a hash sign (#), remove all text following and including this character.
+//     3. Convert all occurences of HT to SPACE.
+//     4. Discard any empty or white-space only lines.
+//
+//     When a response arrives to a controller, it is expected only to do steps 1 and 3 above.
+//
+// We assume input is parsed one line at a time rather than include special newline logic here.
+fn preprocess_line(line: &str) -> Option<String> {
     let mut out = String::new();
+    let mut keep = false;
+
     for c in line.chars() {
         if c == '#' {
             break;
@@ -12,14 +64,17 @@ fn preprocess_line(line: &str) -> Vec<String> {
         if c.is_control() {
             continue;
         }
+        if !keep && !c.is_whitespace() {
+            keep = true;
+        }
         out.push(c);
     }
 
-    out.split_whitespace().map(|s| s.to_owned()).collect()
+    if keep { Some(out) } else { None }
 }
 
 /// A GTP command.
-#[derive(Clone,Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct Command {
     /// A sequence id.
     pub id: Option<u32>,
@@ -32,28 +87,92 @@ pub struct Command {
 impl Command {
     /// Converts a line of input into a Command. Returns None if there was no command.
     pub fn from_line(line: &str) -> Option<Self> {
-        let words = preprocess_line(&line);
-        if words.is_empty() {
-            return None;
-        }
+        let mut id = None;
+        let mut name = String::new();
 
-        let mut command_index = 0;
-        let id = u32::from_str_radix(&words[0], 10).ok();
-        if id.is_some() {
-            command_index += 1;
-        }
-        if command_index >= words.len() {
-            return Some(Command {
-                id: id,
-                name: String::new(),
-                args: Vec::new(),
-            });
-        }
+        preprocess_line(line).map(|line| {
+            let mut words = line.split_whitespace().peekable();
+            if let Some(Ok(command_id)) = words.peek().map(|word| {
+                u32::from_str_radix(word, 10)
+            }) {
+                words.next();
+                id = Some(command_id);
+            }
 
-        Some(Command {
-            id: id,
-            name: words[command_index].clone(),
-            args: words[(command_index + 1)..].to_vec(),
+            // If there was just an id, we will end up with a malformed command, but the GTP
+            // specification does not say how to handle this, so just create the command anyway and
+            // let it fail later with "unknown command".
+
+            if let Some(command_name) = words.next() {
+                name.push_str(command_name);
+            }
+
+            let args: Vec<String> = words.map(|s| s.to_owned()).collect();
+
+            Command {id, name, args}
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::u32;
+
+    use super::*;
+
+    #[test]
+    fn preprocess_line_() {
+        assert_eq!(preprocess_line("1\r2\r quit"), Some("12 quit".to_string()));
+        assert_eq!(preprocess_line("quit # a comment"), Some("quit ".to_string()));
+        assert_eq!(preprocess_line("\tquit\t"), Some(" quit ".to_string()));
+        assert_eq!(preprocess_line("   # a comment"), None);
+        assert_eq!(preprocess_line(""), None);
+    }
+
+    #[test]
+    fn from_line() {
+        assert_eq!(Command::from_line(""), None);
+        assert_eq!(
+            Command::from_line("1"),
+            Some(Command { id: Some(1), name: String::new(), args: Vec::new() })
+        );
+        assert_eq!(
+            Command::from_line("1 quit"),
+            Some(Command { id: Some(1), name: "quit".to_string(), args: Vec::new() })
+        );
+        assert_eq!(
+            Command::from_line("quit"),
+            Some(Command { id: None, name: "quit".to_string(), args: Vec::new() })
+        );
+
+        // If a number is given that is not a u32, it will be treated as a command name.
+        let large_uint = (u32::MAX as u64 + 1).to_string();
+        assert_eq!(
+            Command::from_line(&large_uint),
+            Some(Command { id: None, name: large_uint, args: Vec::new() })
+        );
+        let negative_number = "-1".to_string();
+        assert_eq!(
+            Command::from_line(&negative_number),
+            Some(Command { id: None, name: negative_number, args: Vec::new() })
+        );
+
+        assert_eq!(
+            Command::from_line("play w b19"),
+            Some(Command { id: None, name: "play".to_string(), args: vec!["w".to_string(), "b19".to_string()] })
+        );
+    }
+
+    #[test]
+    fn commands_() {
+        let mut commands = b"one\n2 two\n".commands();
+        assert_eq!(
+            commands.next().unwrap().unwrap(),
+            Command { id: None, name: "one".to_string(), args: Vec::new() }
+        );
+        assert_eq!(
+            commands.next().unwrap().unwrap(),
+            Command { id: Some(2), name: "two".to_string(), args: Vec::new() }
+        );
     }
 }
